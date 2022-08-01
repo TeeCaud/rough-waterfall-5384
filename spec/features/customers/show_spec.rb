984ea3d8657a9268a204ef6a3fee4f26b5ccc47d
Tyler Caudill <teecaud@hotmail.com>
2022-08-01 14:57:05 -0500

    US2

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe 'customer show page' do
     customeritem3 = CustomerItem.create(customer_id: customer2.id, item_id: item3.id)
 
     visit "/customers/#{customer1.id}"
-    save_and_open_page
     expect(current_path).to eq("/customers/#{customer1.id}")
     expect(page).to have_content("Walmart")
     expect(page).to have_content("toothpaste")
@@ -25,5 +24,25 @@ RSpec.describe 'customer show page' do
     expect(page).to_not have_content("Jamie")
     expect(page).to_not have_content("Target")
     expect(page).to_not have_content("TV")
+  end
+
+  it 'has the total price of the customers items' do
+    supermarket1 = Supermarket.create!(name: "Walmart", location: "Chicago")
+    supermarket2 = Supermarket.create!(name: "Target", location: "Atlanta")
+
+    customer1 = Customer.create!(name: "Tyler", supermarket_id: supermarket1.id)
+    customer2 = Customer.create!(name: "Jamie", supermarket_id: supermarket2.id)
+
+    item1 = Item.create!(name: "toothpaste", price: 5)
+    item2 = Item.create!(name: "bread", price: 2)
+    item3 = Item.create!(name: "TV", price: 500)
+
+    customeritem1 = CustomerItem.create!(customer_id: customer1.id, item_id: item1.id)
+    customeritem2 = CustomerItem.create!(customer_id: customer1.id, item_id: item2.id)
+    customeritem3 = CustomerItem.create(customer_id: customer2.id, item_id: item3.id)
+    visit "/customers/#{customer1.id}"
+
+    expect(current_path).to eq("/customers/#{customer1.id}")
+    expect(page).to have_content("Total = 7")
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items}
     it { should have_many(:items).through(:customer_items)}
   end
+
+  describe 'model methods' do
+    it ' has a total price of all of its items' do
+      supermarket1 = Supermarket.create!(name: "Walmart", location: "Chicago")
+      supermarket2 = Supermarket.create!(name: "Target", location: "Atlanta")
+
+      customer1 = Customer.create!(name: "Tyler", supermarket_id: supermarket1.id)
+      customer2 = Customer.create!(name: "Jamie", supermarket_id: supermarket2.id)
+
+      item1 = Item.create!(name: "toothpaste", price: 5)
+      item2 = Item.create!(name: "bread", price: 2)
+      item3 = Item.create!(name: "TV", price: 500)
+
+      customeritem1 = CustomerItem.create!(customer_id: customer1.id, item_id: item1.id)
+      customeritem2 = CustomerItem.create!(customer_id: customer1.id, item_id: item2.id)
+      customeritem3 = CustomerItem.create(customer_id: customer2.id, item_id: item3.id)
+
+      expect(customer1.total_price).to eq(7)
+    end
+  end
 end
